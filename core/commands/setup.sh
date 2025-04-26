722b224b5d7f509e0f666bc0829e683bd407655f
Ege KILIÇ <egekilic@proton.me>
2025-04-26 15:12:41 +0000

    FIX: image detection

--- core/commands/setup.sh
+++ core/commands/setup.sh
@@ -19,7 +19,11 @@ setup() {
     command=$(inipars.get "default" "command")
     data=$(inipars.get "volumes" "data")
     port=$(inipars.get "volumes" "port")
-
+    if [[ "$image" != *:* ]]; then
+        search_image="$image:latest"
+    else
+        search_image="$image"
+    fi
     # Validate required fields
     if [[ -z "$name" || -z "$image" ]]; then
         log.error "Missing required fields in $CONF_FILE: 'name' or 'image'"
@@ -35,7 +39,7 @@ setup() {
     log.setline "$name"
 
     # Pull image if not available locally
-    if ! docker images --format '{{.Repository}}' | grep -q "^$image$"; then
+    if ! docker images --format '{{.Repository}}' | grep -q "^$search_image$"; then
         if docker pull "$image" > "/tmp/$image.pull.log" 2>&1; then
             log.done "$image pulled successfully."
         else
