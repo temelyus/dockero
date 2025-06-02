ef5a4d6a9127268f24dd77438ad8927b119dd8a0
temelyus <egekilic@proton.me>
2025-06-03 00:11:14 +0300

    0x03

--- core/commands/run.sh
+++ core/commands/run.sh
@@ -25,7 +25,7 @@ run() {
     log.sub "Creating new container from image: $image_name"
   else
     log.warn "Image and Container not found. Pulling: $image_name"
-    (docker pull "$image_name" > "/tmp/$image_name.pull.log" && log.done "$image_name installed.") || (log.error "Pulling $image_name" && log.sub "Log: /tmp/$image_name.pull.log" && exit 1)
+    image_clonning || exit 1
   fi
   docker_run
   log.endline "$container_name"
@@ -36,12 +36,22 @@ docker_run() {
     -v /opt/${container_name}:/workspace \
     -p 80 \
     --name "${container_name}" \
-    $( [ -e /dev/snd ] && echo "--device /dev/snd" ) \
-    $( [ -n "$DISPLAY" ] && echo "-e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix" ) \
-    $( [ -n "$WAYLAND_DISPLAY" ] && echo "-e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/xdg/$WAYLAND_DISPLAY -e XDG_RUNTIME_DIR=/tmp/xdg" ) \
-    $( [ -d /run/user/$(id -u)/pulse ] && echo "-v /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse -e PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native" ) \
-    $( command -v nvidia-smi >/dev/null 2>&1 && echo "--gpus all" ) \
+    $([ -e /dev/snd ] && echo "--device /dev/snd") \
+    $([ -n "$DISPLAY" ] && echo "-e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix") \
+    $([ -n "$WAYLAND_DISPLAY" ] && echo "-e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/xdg/$WAYLAND_DISPLAY -e XDG_RUNTIME_DIR=/tmp/xdg") \
+    $([ -d /run/user/$(id -u)/pulse ] && echo "-v /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse -e PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native") \
+    $(command -v nvidia-smi >/dev/null 2>&1 && echo "--gpus all") \
     -v /run/user/$(id -u)/bus:/run/user/$(id -u)/bus \
     "$image_name"
 }
 
+image_clonning() {
+  if docker pull "$image_name" >"/tmp/$image_name.pull.log"; then
+    log.done "$image_name installed."
+    return 0
+  else
+    log.error "Pulling $image_name"
+    log.sub "Log: /tmp/$image_name.pull.log"
+    return 1
+  fi
+}
